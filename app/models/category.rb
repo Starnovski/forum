efbe00e58d2bc6d1a4c499224ee4caab2a5a7a89
Starnovski <pawelsz1818@wp.pl>
2020-10-14 09:16:49 +0200

    Added files upload

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :topics
+  mount_uploader :picture, PictureUploader
 end
---
+++ db/migrate/20201014070947_add_picture_to_category.rb
@@ -0,0 +1,5 @@
+class AddPictureToCategory < ActiveRecord::Migration[5.2]
+  def change
+    add_column :categories, :picture, :string
+  end
+end
