01a0629adc43a4dd13ed864ec4483a1d80d28d8d
Starnovski <pawelsz1818@wp.pl>
2020-10-14 09:27:52 +0200

    Revert "Added files upload"
    This reverts commit efbe00e58d2bc6d1a4c499224ee4caab2a5a7a89.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,3 @@
 class Category < ApplicationRecord
   has_many :topics
-  mount_uploader :picture, PictureUploader
 end
--- db/migrate/20201014070947_add_picture_to_category.rb
+++
@@ -1,5 +0,0 @@
-class AddPictureToCategory < ActiveRecord::Migration[5.2]
-  def change
-    add_column :categories, :picture, :string
-  end
-end
