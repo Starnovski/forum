4dd88737a4f96c03d350925d3b355af768949908
Starnovski <pawelsz1818@wp.pl>
2021-02-09 22:10:50 +0100

    Getting data from my other project(simple code written just to return JSON)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,14 @@
+require 'net/http'
+require 'json'
+
+
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
+    url = 'http://127.0.0.1:8000/posts/posts/'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    @effects = JSON.parse(response)
   end
 
   def show
